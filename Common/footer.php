c8d6f2c47599db114c84fba04eed8591fac20c9e
Aditya <adityasinghw84it@gmail.com>
2022-04-14 12:46:01 +0530

    First

---
+++ Common/footer.php
@@ -0,0 +1,43 @@
+<footer>
+  <div class="footer-top">
+    <div class="container">
+        <div class="row">
+          <div class="col-md-3 col-sm-6 col-xs-12 segment-one md-mb-30 sm-mb-30">
+            <h3>DTU College</h3>
+            <p>This Project has been made by Aditya Singh and Anshul(kuch nhi kia saale ne); with the help of Ms. Trasha Gupta.
+              The purpose of this project is to learn DBMS.
+              I hope you realy enjoyed to visit this project. Thanks!
+            </p>
+          </div>
+          <div class="col-md-2 col-sm-6 col-xs-12 segment-two md-mb-30 sm-mb-30">
+            <h2>Quick Links</h2>
+            <ul>
+              <li><a href="">About DTU</a></li>
+              <li><a href="">Admissions Policy</a></li>
+              <li><a href="">Apply for Admission</a></li>
+              <li><a href="">Download Prospectus</a></li>
+              <li><a href="">Contact Us</a></li>
+            </ul>
+          </div>
+          <div class="col-md-4 col-sm-6 col-xs-12 segment-two md-mb-30 sm-mb-30">
+            <h2>UNDERGRADUATE PROGRAMS</h2>
+            <ul>
+              <li><a href="">B.Sc. Civil Engineering</a></li>
+              <li><a href="">B.Sc. Electrical Engineering</a></li>
+              <li><a href="">B.Sc. Mechanical Engineering</a></li>
+              <li><a href="">B. Architecture</a></li>
+              <li><a href="">BS Computer Science</a></li>
+              <li><a href="">Bachelors of Business Administration</a></li>
+            </ul>
+          </div>  
+          <div class="col-md-3 col-sm-6 col-xs-12 segment-two md-mb-30 sm-mb-30">
+            <h2>CONTACT DETAILS</h2>
+              <p>Address: Shahbad Daulatpur, Main Bawana Road, Delhi, Delhi NCR, 110042</p>
+              <p>Telephones:  011-27871018</p>
+              <p>E-mail: dce.edu</p>
+        </div>
+      </div>
+    </div>
+    </div>
+  <p class="footer-bottom-text"></p>
+</footer>
